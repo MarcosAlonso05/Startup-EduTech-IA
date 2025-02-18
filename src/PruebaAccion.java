3e6974e6d65a8e792d8ba9335b71926dc2b8cabd
Marcos Alonso <marcosalonsoamor05@gmail.com>
2025-02-18 22:02:27 +0100

    prueba realizada

--- src/PruebaAccion.java
+++
@@ -1,9 +0,0 @@
-public class PruebaAccion {
-    public static int add(int a, int b) {
-        return a + b;
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Hola mundo :) esto es una prueba");
-    }
-}
