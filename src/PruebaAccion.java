fd7b2befeaead70b98ee2ce175aeb61649c1961e
Marcos Alonso <marcosalonsoamor05@gmail.com>
2025-02-18 21:53:45 +0100

    Acciones prueba

--- src/PruebaAccion.java
+++ src/PruebaAccion.java
@@ -4,6 +4,6 @@ public class PruebaAccion {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hola mundo :)");
+        System.out.println("Hola mundo :) esto es una prueba");
     }
 }
