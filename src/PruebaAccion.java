fac0cd512e17618b7cbe848b74451bd282bc6cb6
Marcos Alonso <marcosalonsoamor05@gmail.com>
2025-02-18 21:23:33 +0100

    Accion Pruba 2

--- src/PruebaAccion.java
+++ src/PruebaAccion.java
@@ -4,6 +4,6 @@ public class PruebaAccion {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello, GitHub Actions!");
+        System.out.println("Hola mundo :)");
     }
 }
