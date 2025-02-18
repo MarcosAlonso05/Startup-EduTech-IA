7f07b8dd77f9d714ed88df9d8a1a041509dbd195
Marcos Alonso <marcosalonsoamor05@gmail.com>
2025-02-18 15:17:34 +0100

    Prueba de accion 1

---
+++ src/PruebaAccion.java
@@ -0,0 +1,9 @@
+public class PruebaAccion {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub Actions!");
+    }
+}
